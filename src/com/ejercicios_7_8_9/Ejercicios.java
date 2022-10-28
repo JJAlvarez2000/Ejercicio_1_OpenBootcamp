5c35902a804d938babbec2659e8afe1817fdd5cc
Julio <alvarezjjoel6@gmail.com>
2022-10-28 15:51:42 -0300

    ejercicio_tema_7_8_9_V.2

--- src/com/ejercicios_7_8_9/Ejercicios.java
+++ src/com/ejercicios_7_8_9/Ejercicios.java
@@ -1,6 +1,9 @@
 package com.ejercicios_7_8_9;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
 
 public class Ejercicios {
 
@@ -10,6 +13,18 @@ public class Ejercicios {
             reverse += s.charAt(i);
         }
         return reverse;
+    }
+
+    public static int dividirPorCero(int numero) throws ArithmeticException {
+        try {
+            return numero / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Esto no puede hacerse: no se puede dividir por cero");
+            throw e;
+        }
+        finally {
+            System.out.println("Demo de caso: esto se ejecuta siempre");
+        }
     }
 
     public static void main(String[] args) {
@@ -32,5 +47,48 @@ public class Ejercicios {
                 System.out.println("el valor de la posicion " + j + " en la columna " + i + " es: " + intArray[i][j]);
             }
         }
+
+        //Ejercicio 3 con Vector
+        Vector<Integer> vector = new Vector();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        vector.add(4);
+        vector.add(5);
+
+        vector.remove(1);
+        vector.remove(2);
+
+        System.out.println("vector = " + vector);
+
+        //Ejercicio 4
+        // El problema de utilizar un vector con la capacidad por defecto es que
+        // si se supera la capacidad del vector, este se duplica, por lo que se desperdicia memoria.
+
+        //Ejercicio 5
+        ArrayList<String> colores = new ArrayList<>();
+        colores.add("Rojo");
+        colores.add("Verde");
+        colores.add("Azul");
+        colores.add("Amarillo");
+
+        LinkedList<String> colores2 = new LinkedList<>(colores);
+
+        System.out.println("colores = " + colores);
+        System.out.println("colores2 = " + colores2);
+
+        //Ejercicio 6
+        ArrayList<Integer> numeros = new ArrayList<>();
+        for (int j = 1; j < 11; j++) {
+            numeros.add(j);
+        }
+        for (int j = 0; j < numeros.size(); j++) {
+            boolean esImpar = numeros.get(j) % 2 != 0;
+            if (esImpar) numeros.remove(j);
+        }
+        System.out.println("Numeros = " + numeros);
+
+        dividirPorCero(5);
+
     }
 }
